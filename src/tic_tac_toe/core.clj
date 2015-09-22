4bb64b640ca1eeee61491794069c85b8c8774601
Mary Baylis <mary.c.baylis@gmail.com>
2015-09-22 10:16:25 -0400

    Creates fresh board

--- src/tic_tac_toe/core.clj
+++ src/tic_tac_toe/core.clj
@@ -1,12 +1,18 @@
 (ns tic-tac-toe.core
   (:gen-class))
 
-
+(defn create-board
+  []
+  {
+    "a" {"1" nil "2" nil "3" nil}
+    "b" {"1" nil "2" nil "3" nil}
+    "c" {"1" nil "2" nil "3" nil}
+  }
+)
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Welcome to Tic-Tac-Toe!
-    Let's play!")
-)
+  (println "Welcome to Tic-Tac-Toe!")
+  (def board (create-board)))
 
--- test/tic_tac_toe/core_test.clj
+++ test/tic_tac_toe/core_test.clj
@@ -2,6 +2,14 @@
   (:require [clojure.test :refer :all]
             [tic-tac-toe.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+; (deftest a-test
+;   (testing "FIXME, I fail."
+;     (is (= 0 1))))
+
+
+(deftest create-empty-board
+  (testing "Creates an empty board"
+    (is (= (create-board) {"a" {"1" nil "2" nil "3" nil} "b" {"1" nil "2" nil "3" nil} "c" {"1" nil "2" nil "3" nil}}))
+  )
+)
+
