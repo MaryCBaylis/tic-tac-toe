1e92b5ae9387a249b00b37ab87345437e0ca427a
Mary Baylis <mary.c.baylis@gmail.com>
2015-09-22 09:24:28 -0400

    Add welcome message

--- src/tic_tac_toe/core.clj
+++ src/tic_tac_toe/core.clj
@@ -1,7 +1,12 @@
 (ns tic-tac-toe.core
   (:gen-class))
 
+
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println "Welcome to Tic-Tac-Toe!
+    Let's play!")
+)
+
