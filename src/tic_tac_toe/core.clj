14b595d0c95cb552f9e865a008a8e9a7d6d0c479
Mary Baylis <mary.c.baylis@gmail.com>
2015-09-22 14:45:28 -0400

    Asks the user if they want to play

--- src/tic_tac_toe/core.clj
+++ src/tic_tac_toe/core.clj
@@ -13,13 +13,39 @@
 (defn display-board
   [board]
   (println "   1  2  3")
-  (doseq [row board] (print (key row) " ") (doseq [tile (val row)] (print (val tile) " ")) (println))
+  (doseq [row board] 
+    (print (key row) " ") 
+    (doseq [tile (val row)] 
+      (print (val tile) " ")
+    ) 
+    (println)
+  )
+)
+
+(defn play-again?
+  [input]
+  (case (clojure.string/lower-case input)
+    "y" true
+    "yes" true
+    "yep" true
+    "n" false
+    "no" false
+    "nope" false
+  )
 )
 
 (defn -main
   "I play tic-tac-toe!"
   [& args]
   (println "Welcome to Tic-Tac-Toe!")
+  (println "Shall we play a game?  (Y)es/(N)o?")
+
+  (def input (str (read-line)))
+  (if (play-again? input)
+    ;;start the game loop!
+    (println "I've started a game")
+  )
+  (println "See you next time!")
   (def board (create-board))
   (display-board board)
 )
--- test/tic_tac_toe/core_test.clj
+++ test/tic_tac_toe/core_test.clj
@@ -2,14 +2,36 @@
   (:require [clojure.test :refer :all]
             [tic-tac-toe.core :refer :all]))
 
+;Example test
 ; (deftest a-test
 ;   (testing "FIXME, I fail."
 ;     (is (= 0 1))))
 
 
-(deftest create-empty-board
+(deftest test-create-board
   (testing "Creates an empty board"
-    (is (= (create-board) {"a" {"1" nil "2" nil "3" nil} "b" {"1" nil "2" nil "3" nil} "c" {"1" nil "2" nil "3" nil}}))
+    (is (= (create-board) {"a" {"1" " " "2" " " "3" " "} "b" {"1" " " "2" " " "3" " "} "c" {"1" " " "2" " " "3" " "}}))
   )
 )
 
+; My display-board function returns nil.  Try to find a way to test a display?
+; (deftest test-display-board
+;   (testing "Displays board in proper format"
+;     (is (= (display-board (create-board)) "      
+
+;             ")
+;     )
+;   )
+; )
+
+(deftest test-play-again-affirm
+  (testing "Returns true if an affirmative is given"
+    (is (= true (play-again? "y")))
+  )
+)
+
+(deftest test-play-again-neg
+  (testing "Returns false if a negative is given"
+    (is (= false (play-again? "n")))
+  )
+)
